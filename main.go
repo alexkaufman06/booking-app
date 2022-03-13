ea535b86b0781f849fbb2fa285384069da640c08
Alex Kaufman <7607798+alexkaufman06@users.noreply.github.com>
2022-03-13 16:06:35 -0700

    Get user input

--- main.go
+++ main.go
@@ -15,10 +15,22 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", venueTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
 
-	var userName string
+	var userFirstName string
+	var userLastName string
+	var userEmail string
 	var userTickets int
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Println("What is your first name?")
+	fmt.Scan(&userFirstName)
+
+	fmt.Println("What is your last name?")
+	fmt.Scan(&userLastName)
+
+	fmt.Println("What is your email?")
+	fmt.Scan(&userEmail)
+
+	fmt.Println("How many tickets would you like to purchase?")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", userFirstName, userLastName, userTickets, userEmail)
 }
